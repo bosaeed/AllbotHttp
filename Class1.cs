11a9c66cc36e561c30f40d3ef2635e27f003b9d8
abdullah jaber <bin_ali_jaber@hotmail.com>
2023-02-26 21:09:01 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllbotHttp
+{
+    public class Class1
+    {
+    }
+}
