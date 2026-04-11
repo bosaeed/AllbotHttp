9b569e5fd2c1fec06cddc08b1bd96fd2967ba1a8
Abdullah Jaber <bosaeed4@gmail.com>
2026-04-11 22:42:59 +0300

    multiple updates and cleanup

--- Client.cs
+++ Client.cs
@@ -1,11 +1,9 @@
-﻿//using Microsoft.Extensions.Logging;
-using AllbotHttp.Models;
+﻿using AllbotHttp.Models;
 using RestSharp;
 using RestSharp.Authenticators;
 using System;
 using System.Diagnostics;
 using System.Net.Http;
-//using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,7 +22,6 @@ namespace AllbotHttp
         private const string baseUrl = "https://allbot.top/";
         private Routes currentroutes = new Routes();
 
-        //HttpClient client;
         RestClient client;
         JwtAuthenticator authenticator;
         private DateTime tokenTime;
@@ -68,13 +65,6 @@ namespace AllbotHttp
             {
                 Trace.Listeners.Add(traceListner);
             }
-
-
-            //client.BaseAddress = new Uri(baseUrl);
-            //client.DefaultRequestHeaders.Accept.Clear();
-            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //client.DefaultRequestHeaders.Add("Accept", "application/json");
-            //client.Timeout = TimeSpan.FromSeconds(120);
 
 
         }
@@ -162,118 +152,6 @@ namespace AllbotHttp
         }
 
 
-        public async Task<SessionInfo> CreateSessionAsync(SessionInfo sessionInfo)
-        {
-            SessionInfo session = null;
-
-            try
-            {
-                if (!await setToken())
-                {
-                    LogError("can not set token");
-                    return session;
-                }
-
-                //HttpResponseMessage response = await client.PostAsJsonAsync(currentroutes.session_route, sessionInfo);
-                var request = new RestRequest(currentroutes.session_route, Method.Post);
-                request.AddJsonBody(sessionInfo);
-                //request.RequestFormat = DataFormat.Json;
-                //request.AddHeader("Content-Type", "application/json");
-                // The cancellation token comes from the caller. You can still make a call without it.
-                var response = await client.PostAsync<SessionInfo>(request);
-
-                /*Log("response " + response.StatusCode.ToString());
-
-                if (response.StatusCode == HttpStatusCode.Unauthorized)
-                {
-                    token = null;
-                }
-
-                else if (response.StatusCode == HttpStatusCode.InternalServerError)
-                {
-                    Log("Internal server error");
-                }
-
-
-                else if (response.IsSuccessStatusCode)
-                {
-                    session = await response.Content.ReadAsAsync<SessionInfo>();
-                }
-
-                return session;*/
-                return response;
-            }
-            catch (HttpRequestException err)
-            {
-                Log(err.ToString());
-                return session;
-            }
-            catch (Exception err)
-            {
-                Log(err.ToString());
-                return session;
-            }
-
-
-        }
-
-
-        public async Task<SessionInfo> StatusSessionAsync(SessionInfo sessionInfo)
-        {
-            SessionInfo session = null;
-
-            try
-            {
-                if (!await setToken())
-                {
-                    LogError("can not set token");
-                    return session;
-                }
-
-                //HttpResponseMessage response = await client.PostAsJsonAsync(currentroutes.status_route, sessionInfo);
-                var request = new RestRequest(currentroutes.status_route, Method.Post);
-                request.AddJsonBody(sessionInfo);
-
-                var response = await client.PostAsync<SessionInfo>(request);
-
-
-                /* Log("response " + response.StatusCode.ToString());
-                 //Log("error " + response.ReasonPhrase);
-
-                 //remove token if stale or removed or unauthorized
-                 if (response.StatusCode == HttpStatusCode.Unauthorized)
-                 {
-                     token = null;
-                 }
-
-                 else if (response.StatusCode == HttpStatusCode.InternalServerError)
-                 {
-                     Log("Internal server error");
-                 }
-
-
-                 else if (response.IsSuccessStatusCode)
-                 {
-                     session = await response.Content.ReadAsAsync<SessionInfo>();
-                 }
-
-                 return session;*/
-                return response;
-            }
-            catch (HttpRequestException err)
-            {
-                Log(err.ToString());
-                return session;
-            }
-            catch (Exception err)
-            {
-                Log(err.ToString());
-                return session;
-            }
-
-        }
-
-
         public async Task<SubscriptionInfo> SubscriptionInfo(string deviceid)
         {
             SubscriptionInfo subscriptionInfo = null;
@@ -292,25 +170,8 @@ namespace AllbotHttp
 
                 var response = await client.PostAsync<SubscriptionInfo>(request);
 
-                /* Log("response " + response.StatusCode.ToString());
 
-                 if (response.StatusCode == HttpStatusCode.Unauthorized)
-                 {
-                     token = null;
-                 }
-
-                 else if (response.StatusCode == HttpStatusCode.InternalServerError)
-                 {
-                     Log("Internal server error");
-                 }
-
-
-                 else if (response.IsSuccessStatusCode)
-                 {
-                     subscriptionInfo = await response.Content.ReadAsAsync<SubscriptionInfo>();
-                 }
-
-                 return subscriptionInfo;*/
+                 return subscriptionInfo;
                 return response;
             }
             catch (HttpRequestException err)
@@ -342,9 +203,7 @@ namespace AllbotHttp
             var httpContent = new StringContent("{}", Encoding.UTF8, "application/json");
             try
             {
-                //var url = $"{baseUrl}{currentroutes.login_route}?email={email}&password={password}";
-                //Log(url);
-                //HttpResponseMessage response = await client.PostAsync(url, httpContent);
+
 
                 Debug.WriteLine("login *********************************");
                 var request = new RestRequest(currentroutes.login_route, Method.Post);
@@ -354,30 +213,6 @@ namespace AllbotHttp
                 var response = await client.PostAsync<TokenInfo>(request);
 
 
-                /*Log("login in response code :" + response.StatusCode.ToString());
-
-                if (response.StatusCode == HttpStatusCode.Unauthorized)
-                {
-                    if (!isTest)
-                        token = null;
-                    LogError("Unauthorized login in");
-                }
-
-                if (response.IsSuccessStatusCode)
-                {
-                    if (!isTest)
-                    {
-
-                        token = await response.Content.ReadAsAsync<TokenInfo>();
-
-                        SaveToken(token.access_token);
-                        //setToken();
-                        return token.access_token;
-                    }
-                    return "OK";
-                }*/
-
-                //return null;
 
                 if (!isTest)
                 {
@@ -414,38 +249,13 @@ namespace AllbotHttp
                     return mInfo;
                 }
 
-                //HttpResponseMessage response = await client.PostAsJsonAsync(currentroutes.send_message_route, messageInfo);
 
                 var request = new RestRequest(currentroutes.send_message_route, Method.Post);
                 request.AddJsonBody(messageInfo);
 
                 var response = await client.PostAsync<MessageInfo>(request);
 
-                /*Log("response " + response.StatusCode.ToString());
 
-                if (response.StatusCode == HttpStatusCode.Unauthorized)
-                {
-                    token = null;
-                    LogError("unauthorized token");
-                    return mInfo;
-                }
-
-                else if (response.StatusCode == HttpStatusCode.InternalServerError)
-                {
-                    Log("Internal server error");
-                }
-
-
-                else if (response.IsSuccessStatusCode)
-                {
-                    mInfo = await response.Content.ReadAsAsync<MessageInfo>();
-                    mInfo.message = messageInfo.message;
-                    mInfo.mobile = messageInfo.mobile;
-          
-
-                }
-
-                return mInfo;*/
                 return response;
             }
             catch (HttpRequestException err)
@@ -464,13 +274,6 @@ namespace AllbotHttp
         }
 
 
-        public async Task<MessageInfo> SendMessageAsync(string mobile, string msg, bool sendWhats, bool sendSms, bool hide_message = false)
-        {
-
-            var messageInfo = new MessageInfo { mobile = mobile, message = msg, whats = sendWhats, sms = sendSms, hide_message = hide_message };
-            return await SendMessageAsync(messageInfo);
-        }
-
         public async Task<MessageInfo> SendSMSAsync(string mobile, string msg, bool hide_message = false)
         {
 
@@ -478,12 +281,6 @@ namespace AllbotHttp
             return await SendMessageAsync(messageInfo);
         }
 
-        public async Task<MessageInfo> SendWhatsAsync(string mobile, string msg, bool hide_message = false)
-        {
-
-            var messageInfo = new MessageInfo { mobile = mobile, message = msg, whats = true, sms = false, hide_message = hide_message };
-            return await SendMessageAsync(messageInfo);
-        }
         private void SaveToken(String t)
         {
             token = t;
